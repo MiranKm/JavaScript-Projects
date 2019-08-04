bc57070fd5ea287d679d380e9002cd664fc486db
MiranPC <miran9191@gmail.com>
2019-08-05 00:19:24 +0300

    adds the basic model class for the note and inserts it

--- noteApp/app.js
+++ noteApp/app.js
@@ -2,15 +2,42 @@ const noteInput = document.querySelector('.note-input');
 const noteHolder = document.querySelector('.note-holder');
 const insertNoteBtn = document.querySelector('.insert-note');
 
+class Note {
+    constructor() {
+        this.note = []
+    }
+
+
+    addNote(note, id) {
+        const noteItem = {
+            note,
+            id
+        }
+        this.note.push(noteItem)
+    }
+
+    getNotesLength() {
+        return this.note.length;
+    }
+}
 
 const insertNote = note => {
-    const noteMock = `<li class="item">${note}</li>`
+    const noteMock = `<li class="item" item-id="${note.id}">${note.note}  id-> ${note.id}</li>`
     noteHolder.insertAdjacentHTML('afterend', noteMock)
 }
 
+const note = new Note()
 
 insertNoteBtn.addEventListener('click', e => {
     if (noteInput.value) {
-        insertNote(noteInput.value)
+        const newNote = {
+            note: noteInput.value,
+            id: note.getNotesLength() + 1
+        }
+        note.addNote(newNote.note, newNote.id)
+        insertNote(newNote)
+
+        // console.log(note.note.length);
+        noteInput.value = ""
     }
 })
